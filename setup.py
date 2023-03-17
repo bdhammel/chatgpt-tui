a78fb31aeff5064c02d7495c66aecd0530431022
Ben Hammel <bdhammel@gmail.com>
2023-03-16 18:35:01 -0700

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / 'README.md').read_text()
 # This call to setup() does all the work
 setup(
     name="aitui",
-    version="0.0.1",
+    version="0.0.2",
     description="",
     long_description=README,
     long_description_content_type="text/markdown",
